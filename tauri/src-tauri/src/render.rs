bde567f5c3e43fff846fe15f7d0a932c06f6267e
Yuuki <37911889+YuukiRafflesia@users.noreply.github.com>
2023-02-14 19:27:10 +0000

    wav finally rendering!
    other notes include:
    - gave a default value for project file to make devving easier

--- tauri/src-tauri/src/render.rs
+++ tauri/src-tauri/src/render.rs
@@ -1,4 +1,6 @@
-use std::{fs::File, io::Read};
+use std::{fs::File, io::Read, path::Path};
+
+use wav::{BitDepth, Header, WAV_FORMAT_PCM};
 
 use crate::project::Project;
 
@@ -6,7 +8,7 @@ use crate::project::Project;
 pub fn render_project(path: &str) {
     println!("Project File Path: {path}");
 
-    let proj_file = File::open(path);
+    let proj_file = File::open(Path::new(path).join("song.yml"));
     match proj_file {
         Err(e) => eprintln!("Error: Couldn't open project file...\n{e}"),
         Ok(mut f) => {
@@ -22,27 +24,54 @@ pub fn render_project(path: &str) {
 }
 
 fn render_to_file(proj: Project, path: &str) {
-    // TODO: Render to WAV!
-
     let mut all_notes = vec![];
-    for (name, pat) in proj.patterns() {
+    for pat in proj.patterns().values() {
         for note in pat.notes() {
             all_notes.push(note);
         }
     }
 
+    // TODO: Make this work for songs with multiple instruments!
+    let instrument_name = proj.patterns().iter().collect::<Vec<_>>()[0].1.instrument();
+    let used_notes = proj.instruments()[instrument_name].clips();
+
     let mut song_data = vec![];
     for note in all_notes {
-        // TODO: Make this work for songs with multiple instruments!
-        let instrument_name = proj.patterns().iter().collect::<Vec<_>>()[0].1.instrument();
-        let instrument_notes = proj.instruments().iter().collect::<Vec<_>>()[0].1.clips();
-
         let start = note.start_sample(proj.bpm(), proj.sample_rate());
         let end = note.end_sample(proj.bpm(), proj.sample_rate());
+
+        // TODO: Handle these unwraps properly and make sure (idr what I was gonna put here, will write in when I remember)
+        println!("Loading note from path: {}/{}", path, used_notes[note.value()].path());
+        let mut input_note = File::open(Path::new(path).join(used_notes[note.value()].path())).unwrap();
+        let (_in_header, data) = wav::read(&mut input_note).unwrap();
+
+        if data.is_eight() {
+            println!("Wav is 8bit int!");
+        } else if data.is_sixteen() {
+            println!("Wav is 16bit int!");
+        } else if data.is_twenty_four() {
+            println!("Wav is 24bit int!");
+        } else if data.is_thirty_two_float() {
+            println!("Wav is 32bit float!");
+        }
 
         // TODO: Check for clip endings and make it end there if needs be
         
         // TODO: Extend the vec with each note!
-        song_data.extend_from_slice(other);
+        song_data.extend_from_slice(data.as_sixteen().unwrap());
     }
+
+    println!("Song completed! Just rendering now...");
+
+    let out_header = Header::new(
+        WAV_FORMAT_PCM,
+        1,
+        proj.sample_rate(),
+        16,
+    );
+
+    let mut output_song = File::create(Path::new(path).join("out.wav")).unwrap();
+    wav::write(out_header, &BitDepth::Sixteen(song_data), &mut output_song).unwrap();
+
+    println!("Song rendered!");
 }
